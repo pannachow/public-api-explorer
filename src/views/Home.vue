7ebbc224550a1135200efdc4f6be193a0c50f346
pannachow <chowpanna@gmail.com>
2020-12-29 14:44:58 +0200

    add sort

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,23 @@
   <div>
     <table class="table is-hoverable is-striped is-fullwidth">
       <thead>
-        <th>NAME</th>
-        <th>CATEGORY</th>
-        <th>AUTH</th>
-        <th>HTTPS</th>
-        <th>CORS</th>
+        <th v-for="key in Keys" :key="key" @click="sort(key)">
+          {{ key }}
+          <span class="icon" v-if="sortKey === key && sortDir === +1">
+            <i class="fas fa-angle-up"></i>
+          </span>
+          <span class="icon" v-if="sortKey === key && sortDir === -1">
+            <i class="fas fa-angle-down"></i>
+          </span>
+        </th>
       </thead>
       <tbody>
         <tr v-for="api in apis" :key="api.API">
-          <td>
+          <td v-for="key in Keys" :key="key">
             <router-link :to="{ name: 'Detail', params: { name: api.API } }">{{
-              api.API
+              api[key]
             }}</router-link>
           </td>
-          <td>{{ api.Category }}</td>
-          <td>{{ api.Auth }}</td>
-          <td>{{ api.HTTPS }}</td>
-          <td>{{ api.Cors }}</td>
         </tr>
       </tbody>
     </table>
@@ -29,10 +29,22 @@
 import { ref } from "vue";
 import { Api, ApisResponse } from "@/models";
 
+const Keys: (keyof Api)[] = [
+  "API",
+  "Description",
+  "Auth",
+  "HTTPS",
+  "Cors",
+  "Link",
+  "Category"
+];
+
 export default {
   name: "Home",
   setup() {
     const apis = ref<Api[]>([]);
+    const sortKey = ref<string | null>(null);
+    const sortDir = ref<number>(+1);
 
     async function listApis() {
       const response = await fetch("https://api.publicapis.org/entries");
@@ -44,7 +56,16 @@ export default {
     listApis();
 
     return {
-      apis
+      apis,
+      Keys,
+      sortKey,
+      sortDir,
+      sort(key: keyof Api) {
+        const dir = key === sortKey.value ? -sortDir.value : +1;
+        apis.value = apis.value.sort((a, b) => (a[key] > b[key] ? dir : -dir));
+        sortKey.value = key;
+        sortDir.value = dir;
+      }
     };
   }
 };
